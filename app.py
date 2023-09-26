394e4e5e7192e6b23cdaa526ddaa14418bf496e7
Ryan Seeley <ryseeley@gmail.com>
2023-09-26 12:27:16 -0500

    Add valid/invalid before_request handlers

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 from asyncio import sleep
 from typing import Any, Dict
 
-from litestar import Litestar, get
+from litestar import Litestar, Request, get
 
 __all__ = ("async_hello_world", "sync_hello_world")
 
@@ -22,4 +22,17 @@ def sync_hello_world() -> Dict[str, Any]:  # noqa: UP006
     return {"hello": "world"}
 
 
-app = Litestar(route_handlers=[sync_hello_world, async_hello_world])
+def before_request_handler_works(request: Request) -> None:
+    """Before Request Handler."""
+    print("Before Request Handler")
+
+
+def before_request_handler_doesnt_work(request: Request) -> Any:
+    """Before Request Handler."""
+    print("Before Request Handler")
+
+
+app = Litestar(
+    route_handlers=[sync_hello_world, async_hello_world],
+    before_request=before_request_handler_doesnt_work,
+)
